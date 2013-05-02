dd6cd85dce55b766529126400c9e2b713cb4692f
Keith Keller <kkeller@lbl.gov>
2013-05-02 21:30:38 +0000

    Added real models and added to admin interface

--- idmapping/admin.py
+++ idmapping/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from idmapping.models import Poll, Choice
+#from idmapping.models import Poll, Choice, ExternalDB, ExternalID, KBaseID
+from idmapping.models import ExternalDB, ExternalID, KBaseID
 
-admin.site.register(Poll)
-admin.site.register(Choice)
+#admin.site.register(Poll)
+#admin.site.register(Choice)
+admin.site.register(ExternalDB)
+admin.site.register(ExternalID)
+admin.site.register(KBaseID)
--- idmapping/models.py
+++ idmapping/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# TODO: Remove 'Poll' at some point.
 class Poll(models.Model):
     def __unicode__(self):
         return self.question
@@ -11,9 +12,26 @@ class Poll(models.Model):
     pub_date = models.DateTimeField('date published')
     choice = property(_choice)
 
+# TODO: Remove 'Choice' at some point.
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __unicode__(self):
         return self.choice_text
+
+class ExternalDB(models.Model):
+    name        = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    url         = models.CharField(max_length=255)
+
+class ExternalID(models.Model):
+    identifier  = models.CharField(max_length=50)
+    externaldb  = models.ForeignKey(ExternalDB)
+     
+class KBaseID(models.Model):
+    identifier  = models.CharField(max_length=50)
+    externalids = models.ManyToManyField(ExternalID)
+
+     
+
